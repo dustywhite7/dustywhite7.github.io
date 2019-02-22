d7f02e8579b21c4aa9bd95cd4391cde4941d6b5c
Dusty White <dusty.white@gmail.com>
2019-02-22 09:57:06 -0600

    cleaning up game without equations

--- compGameApp/src/Main.elm
+++ compGameApp/src/Main.elm
@@ -77,38 +77,32 @@ view model =
     , div [classList [
         ("industry", True)
         , ("invalid", (Maybe.withDefault 0 (String.toFloat model.q1)) > 0 )
-    ]] [div [] [img [src "apples.png", width 300] [] ]
-            , text "Producers in Apple Market: "
+    ]] [--div [] [img [src "apples.png", width 300] [] ]
+            --, 
+            text "Producers in Apple Market: "
             , input [ placeholder "Students in Industry 1", value model.q1, onInput UpdateQ1 ] []
             , div [] [ text ("Apple Profit: " ++ (Round.round 0 ( (Maybe.withDefault 0 (String.toFloat model.n)) * sqrt (5/(Maybe.withDefault 0 (String.toFloat model.q1))) - (Maybe.withDefault 0 (String.toFloat model.n))/2) ) )  ]
-            , div [classList [
-        ("titspacele", True)
-    ]] [ br [] [] ]
     ]
     , div [classList [
         ("industry", True)
         , ("invalid", (Maybe.withDefault 0 (String.toFloat model.q2)) > 0 )
-    ]] [div [] [img [src "oranges.png", width 300] [] ]
-            , text "Producers in Orange Market: "
+    ]] [--div [] [img [src "oranges.png", width 300] [] ]
+            --, 
+            text "Producers in Orange Market: "
             , input [ placeholder "Students in Industry 2", value model.q2, onInput UpdateQ2 ] []
             , div [] [ text ("Orange Profit: " ++ (Round.round 0 ( (Maybe.withDefault 0 (String.toFloat model.n))/20 * cos ((pi/(Maybe.withDefault 0 (String.toFloat model.n))) * (Maybe.withDefault 0 (String.toFloat model.q2)) ) + (Maybe.withDefault 0 (String.toFloat model.n))/20 ) ) ) ]
-            , div [classList [
-        ("space", True)
-    ]] [ br [] [] ]
     ]
     , div [classList [
         ("industry", True)
         , ("invalid", (Maybe.withDefault 0 (String.toFloat model.q3)) > 0 )
-    ]] [div [] [img [src "bananas.png", width 300] [] ]
-            , text "Producers in Banana Market: "
+    ]] [--div [] [img [src "bananas.png", width 300] [] ]
+            --, 
+            text "Producers in Banana Market: "
             , input [ placeholder "Students in Industry 3", value model.q3, onInput UpdateQ3 ] []
             , div [] [ text ("Banana Profit: " ++ (Round.round 0 ( (3/4) * (Maybe.withDefault 0 (String.toFloat model.n)) - (Maybe.withDefault 0 (String.toFloat model.q3))) ) ) ]
-            , div [classList [
-              ("space", True)
-            ]] [ br [] [] ]
     ]
-    , div [classList [
-        ("industry", True)]] [text "n = total number of students"]
-    , div [classList [
-        ("industry", True)]] [text "x = number of students in market"]
+    -- , div [classList [
+    --     ("industry", True)]] [text "n = total number of students"]
+    -- , div [classList [
+    --     ("industry", True)]] [text "x = number of students in market"]
     ]
